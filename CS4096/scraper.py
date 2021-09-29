9b986a3d4df0859ef074bd6481be2f5c8fbbfc3b
= <lawrencehierlmeier0@gmail.com>
2021-09-29 17:17:13 -0500

    Finalizded Json File creation from webscraping and database creation from json file. Script now fills requirements and full_text fields of the database

--- CS4096/scraper.py
+++ CS4096/scraper.py
@@ -10,6 +10,35 @@ import json
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CS4096.settings")
 
+test_catalog = "Comp Sci"
+degreeAbbreviation = ['Comp Sci', 'Math', 'Comp Eng', 'Philos', 'Stat']
+
+
+def find_all_substring(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1:
+            return
+        yield start
+        start += len(sub)
+
+
+def find_course_prereqs(block):
+    desc = block.find('p', class_="courseblockdesc").text
+    prereq_text = desc[desc.find("Prerequisite"):desc.find("Co-listed")]
+    prereq_text = prereq_text[prereq_text.find('"C"'):]
+    prereq_text = prereq_text[:prereq_text.find('accompanied')]
+    # print(block.find("p", class_="courseblocktitle").find("em").text[15:])
+    for name in degreeAbbreviation:
+        indexes = list(find_all_substring(prereq_text, name))
+        # print(indexes)
+        for index in indexes:
+            # print(prereq_text[index:index + len(name)])
+            # print(prereq_text[index + len(name) + 1:index + len(name) + 5])
+            yield prereq_text[index:index + len(name)], prereq_text[index + len(name) + 1:index + len(name) + 5]
+
+
 if not os.path.exists('courses.json'):
     # Web Scrape
     URL = "https://catalog.mst.edu/undergraduate/degreeprogramsandcourses/computerscience/#courseinventory"
@@ -23,44 +52,40 @@ if not os.path.exists('courses.json'):
     # JSON Creation
     test_catalog = "Comp Sci"
 
+
     data = {"Courses": []}
     for count, course_block in enumerate(course_blocks):
+        prereq_list = list(find_course_prereqs(course_block))
         data["Courses"].append({
             'course_number': test_catalog + " " + course_block.find("p", class_="courseblocktitle").find("em").text[9:13],
             'name': course_block.find("p", class_="courseblocktitle").find("em").text[15:],
             'description': course_block.find('p', class_="courseblockdesc").text,
-            'requirements': "",
+            'requirements': prereq_list,
             'credits': course_block.find("p", class_="courseblocktitle").text[course_block.find("p", class_="courseblocktitle").text.find("(")+5:-3] if len(course_block.find("p", class_="courseblocktitle").text[course_block.find("p", class_="courseblocktitle").text.find("("):]) == 9 else "99",
-            'full_text': ""
+            'full_text': course_block.text
         })
 
     with open('courses.json', 'w') as outfile:
         json.dump(data, outfile)
 
-'''
+
 # Database Creation
-test_catalog = "Comp Sci"
-degreeAbbreviation = ['Comp Sci', 'Math', 'Comp Eng', 'Philos', 'Stat']
+if os.path.exists('courses.json'):
+    print("Pre-DataBase")
 
-def find_course_prereqs(block):
-    desc = block.find('p', class_="courseblockdesc").text
-    print(desc)
+    with open('courses.json') as json_file:
+        data = json.load(json_file)
+        for course_block in data["Courses"]:
+            course = Course(
+                course_number=course_block['course_number'],
+                name=course_block['name'],
+                description=course_block['description'],
+                requirements=course_block['requirements'],
+                credits=course_block['credits'],
+                full_text=course_block['full_text']
+            )
 
-print("Pre-DataBase")
+            course.save()
 
+    print("Post-Database")
 
-for count, course_block in enumerate(course_blocks):
-    if (course_block.find("p", class_="courseblocktitle").find("em").text[15:] != "Special Problems") and (course_block.find("p", class_="courseblocktitle").find("em").text[15:] != "Special Topics"):
-        course = Course(
-            course_number=test_catalog + " " + course_block.find("p", class_="courseblocktitle").find("em").text[9:13],
-            name=course_block.find("p", class_="courseblocktitle").find("em").text[15:],
-            description=course_block.find('p', class_="courseblockdesc").text,
-            requirements="",
-            credits=course_block.find("p", class_="courseblocktitle").text[course_block.find("p", class_="courseblocktitle").text.find("(")+5:-3] if len(course_block.find("p", class_="courseblocktitle").text[course_block.find("p", class_="courseblocktitle").text.find("("):]) == 9 else "99",
-            full_text=""
-        )
-
-        course.save()
-
-print("Post-Database")
-'''
