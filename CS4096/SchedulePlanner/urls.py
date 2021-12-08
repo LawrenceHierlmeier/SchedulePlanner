b6d24f97d4ceef5e0bb8c7e6db062798323697be
Austin Wall <austin@mst.edu>
2021-12-07 21:23:41 -0600

    Fix issues

--- CS4096/SchedulePlanner/urls.py
+++ CS4096/SchedulePlanner/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('course_list/', views.CourseList.as_view(), name='course_list'),
     path('catalog_directory/', views.CatalogDirectory.as_view(), name='catalog_directory'),
     path('catalog/<slug:dept_slug>', views.DeptCourseList.as_view(), name='catalog_dept_course_list'),
-    path('list_courselog', views.ListCourseLog.as_view(), name='list_courselog'),
+    path('list_courselog/', views.ListCourseLog.as_view(), name='list_courselog'),
+    path('add_courselog/', views.add_courselog, name='add_courselog'),
     path('faculty_dashboard/', views.FacultyDashboard.as_view(), name='faculty_dashboard'),
 ]
